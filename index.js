e7f830e46a8b631e188b1697558b4201b45a0d1e
mttble <matthew.beale1@hotmail.com>
2023-08-03 13:25:26 +0930

    implemented mongoose and mongodb integration

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 import express from 'express'
+import mongoose from 'mongoose'
 
 const categories = ['Food', 'Gaming', 'Coding', 'Other']
+
 
 const entries = [
     { category: 'Food', content: 'Pizza is yummy!'},
     { category: 'Coding', content: 'Coding is fun!'},
     { category: 'Gaming', content: 'Skyrim is for the Nords!'}
 ]
+
+mongoose.connect('mongodb+srv://flip242:Vacation1@cluster0.bzqanvn.mongodb.net/journal?retryWrites=true&w=majority')
+    .then(m => console.log(m.connection.readyState === 1 ? 'Mongoose Connected!' : 'Mongoose Failed to connect'))
+    .catch(err => console.error(err))
+
+const entriesSchema = new mongoose.Schema({
+    category: {type: String, required: true},
+    content: {type: String, required: true}
+})
+
+const EntryModel = mongoose.model('Entry', entriesSchema)
 
 const app = express()
 const port = 5501
@@ -28,14 +41,15 @@ app.get('/entries/:id', (req, res) => {
     }
 })
 
-app.post('/entries', (req, res) => {
-    // 1. Retroeve data frp, request (req)
-    console.log(req.body)
-    // 2. Parse/validate it
-    // 3. Push the new entry to the entries array
-    entries.push(req.body)
-    // 4. Send the new entry with 201 status
-    res.status(201).send('Post to /entries')
+app.post('/entries', async (req, res) => {
+    try{
+        const insertedEntry = await EntryModel.create(req.body)
+        res.status(201).send(insertedEntry)
+    }
+    catch (err) {
+        res.status(500).send({ error: err.message })
+    }
+    
 })
 
 app.listen(port)
