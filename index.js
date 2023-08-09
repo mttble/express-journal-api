99bc03cd33045a4bd17603473daa271ddcb5c6d5
mttble <matthew.beale1@hotmail.com>
2023-08-09 11:49:48 +0930

    added cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import { CategoryModel } from './db.js'
 import entryRoutes from './routes/entry_routes.js'
-
+import cors from 'cors'
 
 const app = express()
-const port = 5503
+const port = 5505
+
+app.use(cors())
 
 app.use(express.json())
 
